f4dc608d74022d6f1ac86b3945c835a385173f15
krishnakatneni <krishna.katneni@gmail.com>
2019-01-22 19:59:21 -0500

    Update FileStringifier.java

--- src/edu/fitchburgstate/csc7400/extra/FileStringifier.java
+++ src/edu/fitchburgstate/csc7400/extra/FileStringifier.java
@@ -1,10 +1,18 @@
 
+
 package edu.fitchburgstate.csc7400.extra;
 
 import java.io.*;
+/*FileStringifier is an interface which contains two 
+ * abstract methods display and stringify. 
+ * These methods can be implemented by other class 
+ * that implements this interface. A class that implements 
+ * this interface must implement two methods declared here.
+ */
 
 public interface FileStringifier {
+	//method declaration with PrintWriter as argument.
 	void display(PrintWriter out);
-
+	//method declaration which returns a String.
 	public String stringify();
 }
