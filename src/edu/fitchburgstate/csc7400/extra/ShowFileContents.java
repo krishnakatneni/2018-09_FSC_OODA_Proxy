f83727908e9379c2064e0ae2053a35a5925132a0
krishnakatneni <krishna.katneni@gmail.com>
2019-01-22 20:11:29 -0500

    Initial commit

--- src/edu/fitchburgstate/csc7400/extra/ShowFileContents.java
+++ src/edu/fitchburgstate/csc7400/extra/ShowFileContents.java
@@ -1,29 +1,42 @@
 package edu.fitchburgstate.csc7400.extra;
-
+//import File and PrintWriter classes
 import java.io.File;
 import java.io.PrintWriter;
+
+
 
 public class ShowFileContents {
 
     public static void main(String[] args) {
+    	//Check if args length is not 1, display an error message
         if (args.length != 1) {
             System.out.println("Usage: java ShowFileContents <directory>");
         }
+        //store 0th argument in a string variable
         String dirname = args[0];
-
+        //To create a new File instance with the given pathname string to abstract pathname
         File dir = new File(dirname);
+        /*Tests whether the file or directory denoted by this abstract pathname exists. 
+        If not exists print the error and return*/
         if (!dir.exists()) {
             System.err.println(dirname + " does not exist");
             return;
         }
+        /*Tests whether the file denoted by this pathname is a directory. 
+        If not directory print the error string and return*/
         if (!dir.isDirectory()) {
             System.err.println(dirname + " is not a directory");
             return;
         }
-
+        //Create a PrintWriter object to write on Console
         PrintWriter outWriter = new PrintWriter(System.out);
+        /* Iterate  for all list of files in the directory which is sent as command line argument file by file
+        if file is a directory, don't do any thing, continue loop */
         for (File file: dir.listFiles()) {
         	if (file.isDirectory()) continue;
+        	/* create a SlowFileStringifier object by converting abstract pathname into a pathname string
+        	 * call display method by sending outwriter to display content on console
+        	 */
         	FileStringifier fileStringifier = new SlowFileStringifier(file.getPath());
         	fileStringifier.display(outWriter);
         }
--- src/edu/fitchburgstate/csc7400/extra/SlowFileStringifier.java
+++ src/edu/fitchburgstate/csc7400/extra/SlowFileStringifier.java
@@ -3,44 +3,61 @@ package edu.fitchburgstate.csc7400.extra;
 import java.io.*;
 import java.util.ArrayList;
 
-public class SlowFileStringifier implements FileStringifier {
 
-    public SlowFileStringifier(String filename) {
+
+//class SlowFileStringifier which implements interface FileStringifier
+public class SlowFileStringifier implements FileStringifier {
+	//Constructor method with a String argument to initialize private variable fileName
+	public SlowFileStringifier(String filename) {
         this.fileName = filename;
     }
-
-    public void display(PrintWriter out) {
+	//implementation of interface method to display contents on console using PrintWriter.
+	public void display(PrintWriter out) {
+		//calls stringify() method which returns a String
     	out.println(this.stringify());
     	out.flush();
     }
-    
+    //implementation of interface method which returns a String
     public String stringify() {
         FileReader f;
 		try {
+			/* Create a FileReader object f with the given filename. 
+			It opens given filename in read mode */
 			f = new FileReader(this.fileName);
+			/* Create a BufferedReader object bf with FileReader f. 
+			It creates a buffered character input stream */
 	        BufferedReader bf = new BufferedReader(f);
-	
+	        //Create an ArrayList object.
 	        ArrayList<String> lines = new ArrayList<String>();
 	        String line = null;
+	        /* read a line of text from file via buffered reader and 
+	        assign in a String variable line until end of input stream.*/
 	        while ((line = bf.readLine()) != null) {
+	        	//sleeps current thread for 2000 milli seconds.
 	            Thread.sleep(2000);
+	            //add current line to array list lines
 	            lines.add(line);
 	        }
+	        //close buffered reader object
 	        bf.close();
+	        //join all the lines with a new line delimeter and return to called method.
 	        return String.join("\n", lines);
-        
+        //handle file not found exception, if directory not exists
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			System.err.println("File "+this.fileName+" was not found.");
+		//handle IO exception, if file is not readable
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println("Problem reading file "+this.fileName+".");
+		//handle Interrupted exception, if file is not reading is interrupted
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			System.err.println("File reading interrupted: "+this.fileName);
 		}
 		return "";
     }
+    //Declare a  final variable fileName which cannot be modified  after once assigned.
 
     private final String fileName;
 }
